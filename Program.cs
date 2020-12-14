75658adbcbb4de74dfaf68faeb89456f0e61b396
victorfiz <eliasfizesan@yahoo.com>
2020-12-14 15:37:57 +0000

    Current Shortest Version of Code
    Characters: 688

--- Program.cs
+++ Program.cs
@@ -1,29 +1,20 @@
 ﻿using System;
+class a{
+    static void Main(){
+        
+        string[] x=new string[24]{"first","second","third","fourth","fifth","sixth","seventh","eighth","ninth","tenth","eleventh","twelfth","A partridge in a pear tree.","Two turtle doves,","Three french hens,","Four calling birds,","Five golden rings,","Six geese a-laying,","Seven swans a-swimming,","Eight maids a-milking,","Nine ladies dancing,","Ten lords a-leaping,","Eleven pipers piping,","Twelve drummers drumming,"};
 
-namespace a
-{
-    class b
-    {
-        static void Main()
-        {
-            string[] x = new string[24] {"first","second","third","fourth","fifth","sixth","seventh","eighth","ninth","tenth","eleventh","twelfth","A partridge in a pear tree","Two turtle doves","Three french hens","Four calling birds","Five golden rings","Six geese a-laying","Seven swans a-swimming","Eight maids a-milking","Nine ladies dancing","Ten lords a-leaping","Eleven pipers piping","Twelve drummers drumming"};
+        for(int i=0;i<12;++i){
 
-            for (int i = 0; i < 12; ++i)
-            {
+            Console.Write("On the "+x[i]+" day of Christmas, my true love sent to me\n");
 
-                Console.WriteLine("On the " + x[i] + " day of Christmas, my true love gave to me");
+            int j=i+13;
+            while(j-->12)
+                Console.WriteLine(x[j]);
 
-                int j = i + 13;
-                while (j-- > 12)
-                    Console.WriteLine(x[j]);
 
-                Console.WriteLine();
-
-                if (i == 0)
-                    x[12] = "And a partridge in a pear tree";
-            }
+            if (i<1)
+                x[12] = "And a partridge in a pear tree.\n";
         }
     }
 }
-
-
