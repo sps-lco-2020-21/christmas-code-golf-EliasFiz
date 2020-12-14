35f241ce65ecbb9b51875db612f9490d91af6349
victorfiz <eliasfizesan@yahoo.com>
2020-12-14 11:44:52 +0000

    Working Code
    The following code works perfectly but is not fully condensed

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace christmas_code_golf_EliasFiz
+namespace a
 {
-    class Program
+    class b
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string[] x = new string[24] {"first","second","third","fourth","fifth","sixth","seventh","eighth","ninth","tenth","eleventh","twelfth","A partridge in a pear tree","Two turtle doves","Three french hens","Four calling birds","Five golden rings","Six geese a-laying","Seven swans a-swimming","Eight maids a-milking","Nine ladies dancing","Ten lords a-leaping","Eleven pipers piping","Twelve drummers drumming"};
+
+            for (int i = 0; i < 12; ++i)
+            {
+
+                Console.WriteLine("On the " + x[i] + " day of Christmas, my true love gave to me");
+
+                int j = i + 13;
+                while (j-- > 12)
+                    Console.WriteLine(x[j]);
+
+                Console.WriteLine();
+
+                if (i == 0)
+                    x[12] = "And a partridge in a pear tree";
+            }
         }
     }
 }
+
+
